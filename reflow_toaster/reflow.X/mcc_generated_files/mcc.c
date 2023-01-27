e47cb20792315b52614be9c6ca8091c3ed883a23
uchan-nos <uchan@uchan.net>
2023-01-28 05:53:41 +0900

    MCC再生成に伴う些細な変更

--- reflow_toaster/reflow.X/mcc_generated_files/mcc.c
+++ reflow_toaster/reflow.X/mcc_generated_files/mcc.c
@@ -52,8 +52,8 @@ void SYSTEM_Initialize(void)
     PMD_Initialize();
     PIN_MANAGER_Initialize();
     OSCILLATOR_Initialize();
-    DAC_Initialize();
     FVR_Initialize();
+    DAC_Initialize();
     ADCC_Initialize();
     TMR0_Initialize();
     EUSART_Initialize();
