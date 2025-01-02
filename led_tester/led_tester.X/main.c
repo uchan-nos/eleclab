9943e2bcb39ef09e11c8f05a87489fd12b844507
uchan-nos <uchan@uchan.net>
2025-01-02 20:37:59 +0900

    VDD が正確に 5V でないときに電流値を補正する

--- led_tester/led_tester.X/main.c
+++ led_tester/led_tester.X/main.c
@@ -3,6 +3,8 @@
 #include "mcc_generated_files/mcc.h"
 #include "sc1602.h"
 
+#define FVR_ADC_N 4
+
 void format_dec(char* s, long v, int n, int dp) {
     for (int i = n - 1; i >= 0; --i) {
         if (i == dp) {
@@ -29,14 +31,26 @@ void main(void) {
         //       = adc_value / 50
         // 100I = adc_value * 2
         ADCON1bits.ADPREF = 3; // Vref+ = FVR (1.024V)
-        int current = ADC_GetConversion(AN_SENSE) * 2;
-        // 100Vf = 100(5[V] - adc_value * 5/1024)
-        ADCON1bits.ADPREF = 0; // Vref+ = VDD (5V)
-        int vf = 500 - (ADC_GetConversion(AN_LEDVF) * 500l) / 1024;
+        int current = ADC_GetConversion(AN_SENSE) * 2; // 10uA 単位
 
-        strcpy(s, " 0.00mA Vf=0.00V");
+        ADCON1bits.ADPREF = 0; // Vref+ = VDD (5V)
+        // Vref+ = VDD としたときの FVR の ADC 値（1.024V あたりの ADC 値）
+        int fvr_adc_refvdd = 0;
+        for (int i = 0; i < FVR_ADC_N; i++) { // FVR_ADC_N 回の平均を取る
+          fvr_adc_refvdd += ADC_GetConversion(channel_FVR);
+        }
+        // VDD の電圧 mV を求める
+        // fvr_adc_refvdd/N : Vref+=VDD としたときの、1024mV あたりの ADC 値
+        // 1024 / (fvr_adc_refvdd/N) : ADC の 1 ステップあたりの電圧 mV
+        // 1024 * (1024 / (fvr_adc_refvdd/N)) : フルスケールに相当する電圧 mV
+        int vdd_mv = (1024L * 1024L * FVR_ADC_N) / fvr_adc_refvdd;
+        // LED の VF mV
+        int vf_mv = vdd_mv - (ADC_GetConversion(AN_LEDVF) * (long)vdd_mv) / 1024;
+        int vf = vf_mv / 10; // 10mV 単位
+
+        strcpy(s, " 0.00mA Vf=0.000");
         format_dec(s + 0, current, 5, 2);
-        format_dec(s + 11, vf, 4, 1);
+        format_dec(s + 11, vf_mv, 5, 1);
         lcd_cursor_at(0, 0);
         lcd_puts(s);
 
